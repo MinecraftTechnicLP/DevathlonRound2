504ac1de3c1bfb10179eac472d4753aa917663d3
MinecraftTechnicLP <minecrafttechniclp@gmail.com>
2016-07-22 17:23:13 +0200

    Initialized default stuff

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DevathlonRound2
+
+Project for the second round of [GommeHD.net's Devathlon 2016] (https://www.gommehd.net/forum/threads/devathlon-nummer-3-sei-dabei.501442/)
+
---
+++ src/tk/mctechniclp/devathlon2/Main.java
@@ -0,0 +1,26 @@
+package tk.mctechniclp.devathlon2;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+	
+	@Override
+	public void onEnable() {
+		registerListeners();
+		registerCommands();
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	
+	private void registerListeners() {
+		
+	}
+	
+	private void registerCommands() {
+		
+	}
+}
