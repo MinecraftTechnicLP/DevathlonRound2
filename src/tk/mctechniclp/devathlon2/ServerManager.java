45308ed9ae4d92a8d3d8040fa11b13e4fc2608de
MinecraftTechnicLP <minecrafttechniclp@gmail.com>
2016-07-23 12:01:35 +0200

    Made ServerManager copying template

--- src/tk/mctechniclp/devathlon2/ServerManager.java
+++ src/tk/mctechniclp/devathlon2/ServerManager.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Properties;
 
+import com.google.common.io.Files;
+
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
 
@@ -18,17 +20,39 @@ public class ServerManager {
 	public static void startServer(String name, String motd) {
 		/** Start Bukkit Server */
 		
-		File f = new File("../" + name);
-		if(!f.exists()) {
-			updatePort();
-			f.mkdir();
-		}
+		File serverDir = new File("../" + name);
+		File templateDir = new File("../template");
 		
+		if(!serverDir.exists()) {
+			updatePort();
+			serverDir.mkdir();
+			
+			copyDirectory(templateDir, serverDir);
+			
+		}
 		
 		/** Connect to BungeeCord */
 		ServerInfo serverInfo = ProxyServer.getInstance().constructServerInfo(name, new InetSocketAddress(host, port), motd, false);
 		ProxyServer.getInstance().getServers().put(name, serverInfo);
 		port++;
+	}
+	
+	private static void copyDirectory(File from, File to) {
+		for(File child : from.listFiles()) {
+			File childTo = new File(to.toPath() + "/" + child.getName());
+			
+			if(child.isDirectory()) {
+				childTo.mkdir();
+				copyDirectory(child, childTo);
+				continue;
+			}
+			
+			try {
+				Files.copy(child, childTo);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	private static void updatePort() {
